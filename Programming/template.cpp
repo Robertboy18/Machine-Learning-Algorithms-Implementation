e878ac18c977e2628d90af8bc80195e1a2dcaadd
Robert Joseph <robujsph2001@gmail.com>
2021-01-02 10:34:21 +0400

    Create template.cpp

---
+++ Programming/template.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<string>
+#include<cmath>
+#include<bits/stdc++.h>
+using namespace std;
+
+#define Pi                    3.141592653589793
+#define mod                   1000000007 
+#define INF                   2147483647 
+#define pb                            push_back
+#define minimum(a)                    *min_element(a.begin(), a.end())
+#define maximum(a)                    *max_element(a.begin(), a.end())
+#define accumulate(a)                 accumulate(a.begin(),a.end())
+#define sort(a)                       sort(a.begin(),a.end())
+
+void show(vector<long long int> x)
+{
+    for(auto i:x)
+    {
+        cout<<i<<" ";
+    }
+}
+
+typedef long long int ll;
+typedef vector<ll> ve;
+typedef string s;
+
+
+void soln()
+{
+    ll i,j,x,y;
+
+}
+int main() {
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+    ll x = 1;
+    cin>>x;
+    while(x--) {
+        soln();
+    }
+}
