ddde23192d709371c38a0a60af9a57c3ecfdb8d9
Robert Joseph <robujsph2001@gmail.com>
2021-01-02 11:09:41 +0400

    Create template.py

---
+++ Programming/template.py
@@ -0,0 +1,17 @@
+import sys
+import math
+import itertools
+import random
+import numpy as np
+import collections
+from sys import stdin,stdout
+
+mod = 1e7
+
+sys.setrecursionlimit(100000000)
+
+_ = sys.stdin.readline()
+x =  list(map(str, sys.stdin.readline().strip().split()))
+print(x)
+
+
